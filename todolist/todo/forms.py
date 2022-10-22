58f29a0b6a4cbada9ca629383d654f117863d314
DazzzleKK <krivinsony@gmail.com>
2022-10-22 16:47:03 +0700

    priority badges, priority form

--- todolist/todo/forms.py
+++ todolist/todo/forms.py
@@ -5,8 +5,9 @@ from .models import Tasks
 class NewTaskForm(forms.ModelForm):
     class Meta:
         model = Tasks
-        fields = ['title', 'content']
+        fields = ['title', 'content', 'priority']
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control-sm bg-secondary'}),
+            'priority': forms.Select(attrs={'class': 'form-control-sm bg-secondary'}),
             'content': forms.Textarea(attrs={'class': 'form-control bg-secondary', 'rows': 5}),
         }
