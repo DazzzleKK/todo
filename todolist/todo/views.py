1fc0c22c7625ab7870acd1755fb2a0bb78901ed2
DazzzleKK <krivinsony@gmail.com>
2022-11-14 13:46:15 +0700

    pretify? +navbar

--- todolist/todo/views.py
+++ todolist/todo/views.py
@@ -44,6 +44,11 @@ class ToDoList(ListView):
         else:
             return None
 
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['tasks'] =  context['tasks'].filter(author=self.request.user)
+    #     return context
+
 
 class TaskEditView(UpdateView):
     model = Tasks
@@ -74,6 +79,10 @@ class TaskCreateView(CreateView):
         obj = form.save(commit=False)
         obj.author = self.request.user
         return super(TaskCreateView, self).form_valid(form)
+
+    # def form_valid(self, form):
+    #     form.instance.author = self.request.user
+    #     return super(TaskCreateView, self).form_valid(form)
 
 
 class TaskDeleteView(DeleteView):
---
+++ todolist/todolist/static/js/buttons.js
@@ -0,0 +1,13 @@
+// const buttons = document.getElementsByClassName('accordion-button')
+
+
+// for (const button of buttons) {
+//     button.addEventListener('click', e =>{
+//         if (!button.innerHTML.includes('opened')){
+//             button.innerHTML += '<b>(opened)</b>'
+//         } else {
+//             button.innerHTML = button.innerHTML.replace('(opened)', '')
+//         }
+        
+//     })
+// }
